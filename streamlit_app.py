4a1de85f277614f9343e107b290640c7f3ad2df0
BakhtiyarKh <->
2024-12-10 18:50:13 +0300

    updated async request handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,32 @@
 import streamlit as st
-from openai import OpenAI
+import requests
 
 # Show title and description.
 st.title("💬 Chatbot")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "This is a simple chatbot that uses google-bert/bert-base-multilingual-cased model to generate responses. "
+    "To use this app, you need to provide an google-bert/bert-base-multilingual-cased API key, which you can get on huggingface website. "
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+def is_nested_list(variable):
+    # Проверить, является ли переменная списком
+    if isinstance(variable, list):
+        # Проверить, содержит ли список только другие списки
+        return all(isinstance(item, list) for item in variable)
+    return False
+
+# Ask user for their google-bert/bert-base-multilingual-cased API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+# bert_api_key = st.text_input("google-bert/bert-base-multilingual-cased API Key", type="password")
+bert_api_key = st.text_input("google-bert/bert-base-multilingual-cased API Key", value="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", type="password")
+if not bert_api_key:
+    st.info("Please add your API key to continue.", icon="🗝️")
 else:
-
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
-
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
     if "messages" not in st.session_state:
@@ -32,25 +39,33 @@ else:
 
     # Create a chat input field to allow the user to enter a message. This will display
     # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+    if prompt := st.chat_input("The answer to the universe is [MASK]."):
 
         # Store and display the current prompt.
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.markdown(prompt)
 
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-
+        API_URL = "https://api-inference.huggingface.co/models/google-bert/bert-base-multilingual-cased"
+        api_key_0 = "Bearer " + bert_api_key
+        headers = {"Authorization": api_key_0}
+        
         # Stream the response to the chat using `st.write_stream`, then store it in 
         # session state.
         with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+            # Query the API
+            response = query({"inputs": prompt})
+            result = ""
+
+            # Process the response
+            if is_nested_list(response):
+                for item in response:
+                    result += item[0]["sequence"]
+                    result += ". \n"
+            else:
+                result += response[0]["sequence"]
+
+            # Add result to messages
+            st.session_state.messages.append({"role": "assistant", "content": result})
+            # Display the assistant's response
+            st.markdown(result)
